271ad64d93610125f9db00ecf96d4e158162d2f3
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-08 00:38:09 -0400

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,20 +1,36 @@
+#!/bin/sh
 cd $(dirname $0)
+
 cd ../complete
-mvn package
-java -jar target/gs-rest-service-complete-0.1.0.jar &
-PID=$!
-sleep 3
-curl -s http://localhost:8080/greeting > target/actual.json
-kill -9 $PID
 
-if diff -w ../test/expected.json target/actual.json
-	then
-		echo SUCCESS
-		let ret=0
-	else
-		echo FAIL
-		let ret=255
-fi
-
-rm -rf target
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
 exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+cd ../initial
+
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+exit
