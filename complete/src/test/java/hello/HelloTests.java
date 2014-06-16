21f0c713858ed33607604bccd67b72ed952ec61c
Dave Syer <dsyer@gopivotal.com>
2014-06-16 17:07:52 +0100

    Use JsonPath instead of Greeting on the client

--- complete/src/test/java/hello/HelloTests.java
+++ complete/src/test/java/hello/HelloTests.java
@@ -1,23 +1,20 @@
 
 package hello;
 
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -29,17 +26,10 @@ public class HelloTests {
 	@Value("${local.server.port}")
 	private int port;
 
-	@Autowired
-	private HttpMessageConverters converters;
-
 	@Test
-	public void envEndpointNotHidden() {
-		TestRestTemplate template = new TestRestTemplate();
-		template.setMessageConverters(converters.getConverters());
-		ResponseEntity<Greeting> response = template.exchange("http://localhost:"
-				+ this.port + "/greeting", HttpMethod.GET, new HttpEntity<Void>(
-				(Void) null), Greeting.class);
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertNotNull(response.getBody().getContent());
+	public void envEndpointNotHidden() throws Exception {
+		Traverson traverson = new Traverson(new URI("http://localhost:" + this.port + "/greeting"), MediaTypes.HAL_JSON);
+		String greeting = traverson.follow("self").toObject("$.content");
+		assertEquals("Hello, World!", greeting);
 	}
 }
