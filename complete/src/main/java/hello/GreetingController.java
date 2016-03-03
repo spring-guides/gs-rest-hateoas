0458623e21ccb7d3b762909f30697b5963f52ea6
Matthias Sieber <matthiasksieber@gmail.com>
2016-03-03 14:22:25 -0800

    Small source and readme changes
    This is a combination of 3 commits.
    The first commit's message is:
    Added to explanation on accepting only GET request
    
    This is the 2nd commit message:
    
    Simplify GreetingController
    
    replaced Controller with RestController and removed ResponseBody
    annotation
    
    This is the 3rd commit message:
    
    modified README to reflect the simple changes in the source

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -5,18 +5,16 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
 
     @RequestMapping("/greeting")
-    @ResponseBody
     public HttpEntity<Greeting> greeting(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 
