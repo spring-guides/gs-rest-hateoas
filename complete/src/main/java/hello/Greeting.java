ffcdd85840904a5e52ba54348c2dfd34ca255045
Szabolcs Vasas <vasas@apache.org>
2019-12-02 17:15:21 +0700

    Parameterizing the reference to RepresentationModel with Greeting.

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -5,7 +5,7 @@ import org.springframework.hateoas.RepresentationModel;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Greeting extends RepresentationModel {
+public class Greeting extends RepresentationModel<Greeting> {
 
 	private final String content;
 
