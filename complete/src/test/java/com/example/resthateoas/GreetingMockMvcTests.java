2b74d71b465a39dcd576d9088ba27df68151eded
Jay Bryant <jbryant@pivotal.io>
2020-01-15 13:22:57 -0600

    Upgrade to Spring Boot 2.2
    The Spring HATEOAS library had a breaking change that mandated updating to Spring Boot 2.2. Doing so also mandated changing the tests to Junit 5, which, in turn, mandated chaning the Gradle wrapper.

--- complete/src/test/java/com/example/resthateoas/GreetingMockMvcTests.java
+++ complete/src/test/java/com/example/resthateoas/GreetingMockMvcTests.java
@@ -19,15 +19,15 @@ package com.example.resthateoas;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GreetingMockMvcTests {
--- initial/src/test/java/com/example/resthateoas/RestHateoasApplicationTests.java
+++ initial/src/test/java/com/example/resthateoas/RestHateoasApplicationTests.java
@@ -1,11 +1,11 @@
 package com.example.resthateoas;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class RestHateoasApplicationTests {
 
