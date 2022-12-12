a867f6a02583c59db89e7eda578e9367a1d93924
Jay Bryant <jbryant@vmware.com>
2022-12-12 10:28:02 -0600

    Upgraded to Spring Boot 3.0.0
    to get onto the new major version of Spring Boot.

--- complete/src/test/java/com/example/resthateoas/GreetingTraversonIntegrationTests.java
+++ complete/src/test/java/com/example/resthateoas/GreetingTraversonIntegrationTests.java
@@ -25,15 +25,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.client.Traverson;
+import org.springframework.beans.factory.annotation.Value;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class GreetingTraversonIntegrationTests {
 
-	@LocalServerPort
+	@Value(value="${local.server.port}")
 	private int port;
 
 	@Test
